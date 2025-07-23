0da3eabba99df4c6fe5c71e116b66cc7bb99b6d7
ERICK ORTIZ VELÁSQUEZ <erick.ortiz.program@gmail.com>
2025-07-23 19:16:46 -0400

    update commit deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    API_DOMAINS: {
+        // API_URL_INCOMES: 'http://localhost:3000/api/incomes',
+        // API_URL_EXPENSES: 'http://localhost:3000/api/expenses'
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    API_DOMAINS: {
+        // API_URL_DASHBOARD: 'http://localhost:3000/api/dashboard',
+        // API_URL_INCOMES: 'http://localhost:3000/api/incomes',
+        // API_URL_EXPENSES: 'http://localhost:3000/api/expenses'
+    }
+};
